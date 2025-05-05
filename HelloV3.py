1b9540742c02e2b5cd2580edeccf29a6f5ccb9c9
AdrianQFA <adacosta@ugr.es>
2025-05-05 17:13:13 +0200

    Version 7 del código

--- HelloV3.py
+++ HelloV3.py
@@ -1,3 +1,4 @@
 "hago ahora este comentario"
 print ("comentario despues del tag")
 print ("comentario depues de crear la rama login")
+print ("comentario depues de unir la rama main a la rama login")

6e65dda94a580fea163022133e8a27e510a28100
AdrianQFA <adacosta@ugr.es>
2025-05-05 17:40:06 +0200

    Version  8 del código

--- HelloV3.py
+++ HelloV3.py
@@ -1,8 +1,5 @@
 "hago ahora este comentario"
 print ("comentario despues del tag")
 print ("comentario depues de crear la rama login")
-<<<<<<< HEAD
-print ("Soy del equipo de login y estoy haciendo un cambio en un archivo que no me corresponde")
-=======
 print ("comentario depues de unir la rama main a la rama login")
->>>>>>> master
+print ("comentario depues de unir login con main")

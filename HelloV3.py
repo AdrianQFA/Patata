7bd2677e7eccbb0c488d4f7a396bf6cc708b57ce
AdrianQFA <adacosta@ugr.es>
2025-05-05 17:16:23 +0200

    rama login ha cambiado un archivo que no corresponde

--- HelloV3.py
+++ HelloV3.py
@@ -1,3 +1,4 @@
 "hago ahora este comentario"
 print ("comentario despues del tag")
 print ("comentario depues de crear la rama login")
+print ("Soy del equipo de login y estoy haciendo un cambio en un archivo que no me corresponde")
